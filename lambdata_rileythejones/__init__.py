362ded9a74381261c32326c28d79fe39df440233
rileythejones <rileymjones@gmail.com>
2019-12-02 15:34:26 -0700

    test push

---
+++ lambdata_rileythejones/__init__.py
@@ -0,0 +1,14 @@
+"""lambdata - collection of data science hwlper functions """
+
+import pandas as pd 
+import numpy as np 
+
+# sample code 
+
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(50))
+
+# sample functions 
+
+def increment(x)
+    return (x + 1)
---
+++ lambdata_rileythejones/df_utils.py
@@ -0,0 +1,8 @@
+""""
+utility functions for working with DataFrames
+
+"""
+
+import pandas 
+
+TEST_DF = pandas.DataFrame([1, 2, 3, 4, 5])
