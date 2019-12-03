bdeeda3189df31957a1c69bba7c76f753f41cf91
rileythejones <rileymjones@gmail.com>
2019-12-02 17:03:35 -0700

    redid lecture

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ REQUIRED = [
 with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
     setuptools.setup(
-    name="lambdata-DS9",
+    name="lambdata_rileythejones",
     version = "0.1.1",
     author = "rileythejones",
     description = "a collection of data science helper functions",
