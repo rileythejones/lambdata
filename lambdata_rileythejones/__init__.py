d9478292e2db8543deb2699ae524519d3a744601
rileythejones <rileymjones@gmail.com>
2019-12-02 18:36:15 -0700

    added two functions

--- lambdata_rileythejones/__init__.py
+++ lambdata_rileythejones/__init__.py
@@ -12,3 +12,17 @@ ZEROS = pd.DataFrame(np.zeros(50))
 
 def increment(x):
     return (x + 1)
+
+
+df = pd.DataFrame([1, 2, 3, 4, 5, 6, 7, 8, 9, np.NaN, 0, 0])
+
+def check_nulls(df):
+  result = df.isnull().sum().sum()
+  return print('There are', result, 'null values in this dataframe')
+
+# works for a single column 
+
+def bag_tag(data, segments=10):
+  edge = segments + 1
+  labels = range(1, edge, 1) 
+  return pd.qcut(data, q=segments, labels=labels)
