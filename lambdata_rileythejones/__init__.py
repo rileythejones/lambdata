e6949002d249f6a829eecb57325c5e394f974595
rileythejones <rileymjones@gmail.com>
2019-12-03 20:34:01 -0700

    created a class with functions

--- lambdata_rileythejones/__init__.py
+++ lambdata_rileythejones/__init__.py
@@ -1,28 +1,36 @@
-"""lambdata - a collection of data science helper functions """
+"""lambdata_rileythejones - a collection of data science helper functions """
 
 import pandas as pd 
 import numpy as np 
+from scipy import stats
 
-# sample code 
+class CleanData:
+    """
+    functions to clean a dataset
+    """
+    def __init__(self, df):
+        self.df = df
+    """
+    returns the total number of null values in the entire dataframe
+    """
+    def check_nulls(self):
+      result = self.df.isnull().sum().sum()
+      return print('There are', result, 'null values in this dataframe')
 
-ONES = pd.DataFrame(np.ones(10))
-ZEROS = pd.DataFrame(np.zeros(50))
+    """
+    removes rows that have at least one value that is n-standard deviations from a column mean
+    """
 
-# sample functions 
+    def outlier_filter(self, deviations=2):
+      return self.df[(np.abs(stats.zscore(self.df)) < deviations).all(axis=1)]
 
-def increment(x):
-    return (x + 1)
+    """
+    takes an array of data as an input and outputs integer labels the correspond to 
+    proportional bin rank 
+    """
 
+    def bag_tag(self, data, segments=10):
+      edge = segments + 1
+      labels = range(1, edge, 1) 
+      return pd.qcut(data, q=segments, labels=labels)
 
-df = pd.DataFrame([1, 2, 3, 4, 5, 6, 7, 8, 9, np.NaN, 0, 0])
-
-def check_nulls(df):
-  result = df.isnull().sum().sum()
-  return print('There are', result, 'null values in this dataframe')
-
-# works for a single column 
-
-def bag_tag(data, segments=10):
-  edge = segments + 1
-  labels = range(1, edge, 1) 
-  return pd.qcut(data, q=segments, labels=labels)
--- lambdata_rileythejones/df_utils.py
+++ lambdata_rileythejones/df_utils.py
@@ -3,6 +3,11 @@ utility functions for working with DataFrames
 
 """
 
-import pandas 
+import pandas as pd 
+import numpy as np 
 
-TEST_DF = pandas.DataFrame([1, 2, 3, 4, 5])
+df_null = pd.DataFrame([1, 2, 3, 4, 5, 6, 7, 8, 9, np.NaN, 0, 0])
+
+df_random = pd.DataFrame(np.random.randn(100, 3))
+
+df_random_column = df_random[0]
